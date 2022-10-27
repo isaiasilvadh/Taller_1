25ea35d036a5db524a4c7b49a70a2f3482d14dff
isaiasilvadh <isaiasilva64@gmail.com>
2022-10-27 16:46:44 -0500

    Initial commit

---
+++ app/src/main/java/com/example/taller_1/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.taller_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mayorEdad()
+        multiplicar()
+        list()
+        autos()
+    }
+
+    fun mayorEdad() {
+        var edad = 20
+
+        if (edad >= 18) {
+            println("Usted es mayor de edad")
+        } else {
+            println("Usted no es mayor de edad")
+        }
+    }
+
+    fun multiplicar() {
+        val num = 5
+
+        for (i in 1..12) {
+            val resul = num * i
+            println("$num * $i = $resul")
+        }
+        for (j in 12 downTo 1) {
+            val resul2 = num * j
+            println("$num * $j = $resul2")
+        }
+    }
+
+    data class listado(var nombre: String, var subGrupo: Int)
+
+    fun list() {
+        val nombres = mutableListOf(
+            listado("Isaias", 2), listado("Pablo", 2),
+            listado("Anthonny", 3), listado("Carlos", 4)
+        )
+        nombres.sortBy { it.subGrupo }
+        nombres.forEach { (println(it)) }
+    }
+    class autos{
+        var apellido: String = ""
+        var edad: Int = 0
+
+    }
+
+
+
+}
